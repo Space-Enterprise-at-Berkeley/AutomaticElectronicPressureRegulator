18c8b88ea467cef746202d1de617be533a6d780f
vint <vint@berkeley.edu>
2021-11-07 00:09:02 -0700

    new test data

--- src/ElectronicRegulator.cpp
+++ src/ElectronicRegulator.cpp
@@ -9,7 +9,8 @@
 
 #define MAX_SPD 255
 #define MIN_SPD -255
-#define STATIC_SPD 60
+// #define STATIC_SPD 60
+#define STATIC_SPD 0
 
 #define MAX_ANGLE 1200
 #define MIN_ANGLE 0
@@ -18,7 +19,7 @@
 #define HP_PT A1
 #define LP_PT A2
 
-#define BUFF_SIZE 50
+#define BUFF_SIZE 30
 
 // Change these two numbers to the pins connected to your encoder.
 //   Best Performance: both pins have interrupt capability
@@ -127,7 +128,7 @@ void ptTest() {
     // print PT reading 6 times, at 0.5s intervals
     Serial.println("Starting PT test...");
     long lastPrint = 0;
-    Buffer p_buff(50);
+    Buffer p_buff(BUFF_SIZE);
     float old_p = 0;
     float p = 0;
     long t = 0;
@@ -264,7 +265,93 @@ int waitConfirmation(){
     }
 }
 
+void servoCharacterization() {
+    Serial.println("Starting servo-based characterization...");
+    long angle;
+    bool isAngleUpdate;
+    long oldPosition=-999;
+    long e=0;
+    long oldError=0;
 
+    long setPoint=0;
+    // float kp=11.5;
+    // float ki=1.5e-6;
+    // float kd=0.1665e6;
+
+    float kp=11.5;
+    float ki=1.5e-6;
+    float kd=0.21e6;
+
+    long errorInt=0;
+    unsigned long t2;
+    unsigned long dt;
+    bool isPrint = true;
+    unsigned long lastPrint = 0;
+
+    unsigned long flowStart = millis(); // in millis
+    unsigned long flowDuration = 2500;
+
+    unsigned int printFreq = 100; // in millis
+
+    String inString="";
+
+    while (true) {
+        dt=micros()-t2;
+        t2+=dt;
+        angle = encoder.read();
+        isAngleUpdate=(angle!=oldPosition);
+        e=angle-setPoint;
+        //PI control
+        float rawSpd=-(kp*e+kd*(e-oldError)/float(dt));
+        if(rawSpd<MAX_SPD && rawSpd>MIN_SPD){ //anti-windup
+            errorInt+=e*dt;
+            rawSpd-=ki*errorInt;
+        }
+        else{errorInt=0;}
+        rawSpd += ((rawSpd<0) ? -STATIC_SPD : STATIC_SPD);
+        speed=min(max(MIN_SPD,rawSpd),MAX_SPD);
+        runMotor();
+        if (isPrint && (millis()-lastPrint > printFreq)){
+            Serial.println(String(millis()) + "\t" + String(speed)+"\t"+String(angle)+"\t"+String(setPoint) + "\t" + String(voltageToPressure(analogRead(HP_PT))) + "\t" + String(voltageToPressure(analogRead(LP_PT))));
+            lastPrint = millis();
+        }
+
+        if (millis()-flowStart > flowDuration) {
+            setPoint = 0;
+            printFreq = 5000;
+        }
+        
+        while (Serial.available() > 0) {
+            //Read incoming commands
+            int inChar = Serial.read();
+            
+            if (inChar == '\n') {
+                if (inString == "fin"){
+                    return;
+                } else if (inString == "quiet"){
+                    isPrint = false;
+                } else if (inString == "loud"){
+                    isPrint = true;
+                } else{
+                    // Start a flow
+                    isPrint = true;
+                    setPoint=inString.toInt();
+                    flowStart = millis();
+                    printFreq = 100;
+                }
+                inString = "";
+            } else {
+                inString += (char)inChar;
+            }
+            
+        }
+        if (isAngleUpdate) {
+            oldPosition = angle;
+            
+        }
+        oldError=e;
+    }
+}
 
 long angle;
 bool isAngleUpdate;
@@ -287,8 +374,8 @@ long angle_errorInt=0;
 // perhaps run filtering on pressure derivative
 double kp_outer = 40;
 double ki_outer = 30.0e-6;
-double kd_outer = 2.5e6;
-double pressure_setpoint = 100;
+double kd_outer = 0.5;
+double pressure_setpoint = 0; //100
 double pressure_e = 0;
 double pressure_e_old = 0;
 double pressure_errorInt = 0;
@@ -306,23 +393,24 @@ void setup() {
 
     waitConfirmation();
     // move to close motors
-    // speed = 200;
-    // runMotor();
-    // delay(1000);
-    // speed = 0;
-    // runMotor();
-
+    Serial.println("Zeroing valve");
+    speed = -150;
+    runMotor();
+    delay(1000);
+    speed = 0;
+    runMotor();
     // zero encoder value (so encoder readings range from -x (open) to 0 (closed))
     encoder.write(0);
 
-    // waitConfirmation();
+    waitConfirmation();
     // motorDirTest();
-    // ptTest();
+    ptTest();
     // motorPowerTest();
-    // waitConfirmation();
+    Serial.println("Next input will start servo loop, starting setpoint = "+String(pressure_setpoint));
+    waitConfirmation();
     // potTest();
-    servoTest();
-    exit(0);
+    // servoTest();
+    // servoCharacterization();
     t2 = micros();
 }
 
@@ -361,7 +449,7 @@ void loop() {
     pressure_e = LPpsi - pressure_setpoint;
     p_buff->insert(t2/1.0e6, LPpsi);
     double rawAngle = -( kp_outer*pressure_e + kd_outer*(p_buff->get_slope()) );
-    if(rawAngle<MAX_ANGLE && rawAngle>MIN_ANGLE){
+    if(rawAngle<MAX_ANGLE && (rawAngle>MIN_ANGLE || pressure_errorInt<0)){
         pressure_errorInt += pressure_e * dt;
         rawAngle -= ki_outer * pressure_errorInt;
     }
@@ -374,22 +462,22 @@ void loop() {
     runMotor();
 
     if (t2 - lastPrint > 50) {
-        Serial.println( String(t2) + "\t"+ String(angle_setpoint) + "\t"+ String(pressure_setpoint) +"\t" + String(speed) + "\t" + String(motorAngle) + "\t" + String(HPpsi) + "\t" + String(LPpsi) + "\t" + String(p_buff->get_slope()) );
+        Serial.println( String(t2) + "\t"+ String(angle_setpoint) + "\t"+ String(pressure_setpoint) +"\t" + String(speed) + "\t" + String(motorAngle) + "\t" + String(HPpsi) + "\t" + String(LPpsi) + "\t" + String(p_buff->get_slope()) + "\t" + String(pressure_errorInt) );
         lastPrint = millis();
     }
 
-    // while (Serial.available() > 0) {
-    //     //Read incoming commands
-    //     int inChar = Serial.read();
+    while (Serial.available() > 0) {
+        //Read incoming commands
+        int inChar = Serial.read();
         
-    //     if (inChar == '\n') {
-    //         angle_setpoint=inString.toInt();
-    //         inString = "";
-    //     } else {
-    //         inString += (char)inChar;
-    //     }
+        if (inChar == '\n') {
+            pressure_setpoint=inString.toInt();
+            inString = "";
+        } else {
+            inString += (char)inChar;
+        }
         
-    // }
+    }
 
 }
 
