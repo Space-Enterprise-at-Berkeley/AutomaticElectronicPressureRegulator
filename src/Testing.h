3a8af8dd81336524caa740c0c8ed63197f34cb56
Minh Nguyen <retrominh@gmail.com>
2022-08-01 21:43:20 -0700

    Setting up framework for cart setup

--- src/Testing.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include <Util.h>
-#include <HAL.h>
-
-namespace Testing {
-    //TODO move to test folder
-}
--- src/Util.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Util {
-    double encoderToAngle(double encoderValue);
-    double voltageToPressure(double voltage);
-    double voltageToHighPressure(double voltage);
-    double readPot();
-    double compute_feedforward(double pressure_setpoint, double hp);
-}
