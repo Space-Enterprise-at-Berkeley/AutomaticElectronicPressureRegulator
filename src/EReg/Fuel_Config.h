1f29c16a79c0410ee9797a3ed3fca5361654210d
vint <vint@berkeley.edu>
2022-09-01 02:56:54 -0700

    decreased fuel inner d

--- src/EReg/Fuel_Config.h
+++ src/EReg/Fuel_Config.h
@@ -5,7 +5,7 @@ namespace Config {
 
     // Controller Constants
     const double p_outer_nominal = 1.0, i_outer_nominal = 0.7e-6, d_outer_nominal = 0.06; // nominal is 4000 -> 500 psi flow
-    const double p_inner = 11, i_inner = 3.5e-6, d_inner = 0.2;
+    const double p_inner = 11, i_inner = 3.5e-6, d_inner = 0.1;
 
     // Flow Parameters
     const float pressureSetpoint = 490;
