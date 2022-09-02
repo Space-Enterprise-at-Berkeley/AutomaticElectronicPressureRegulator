8875fd709de23fb161c2a01fa5b14f398d09a6fc
Minh Nguyen <retrominh@gmail.com>
2022-09-01 18:09:13 -0700

    enabling igniter aborts

--- src/AC/AC1/Automation.h
+++ src/AC/AC1/Automation.h
@@ -15,7 +15,7 @@ namespace Automation {
     const float breakWireThreshold = 20;
     const float twoWayCurrentThreshold = 0.2;
 
-    const long burnTime = 15 * 1e6 + 1e6;
+    const long burnTime = 35 * 1e6;
 
     extern bool igniterEnabled;
     extern bool breakwireEnabled;
