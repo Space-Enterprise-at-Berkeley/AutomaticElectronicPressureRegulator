0e654efb33e78c7444e3914a0ba3a45cebe55e7d
Minh Nguyen <retrominh@gmail.com>
2022-10-05 02:34:39 -0700

    new constants

--- src/EReg/Ducers.cpp
+++ src/EReg/Ducers.cpp
@@ -6,17 +6,27 @@ namespace Ducers {
 
     }
 
+    float interpolate1000(uint16_t rawValue) {
+        float tmp = (float) (rawValue - 6406);
+        return tmp / 51.7;
+    }
+
+    float interpolate5000(uint16_t rawValue) {
+        float tmp = (float) rawValue;
+        return tmp / 12.97;
+    }
+
     float readHPPT() {
         int16_t adc0 = HAL::adc.readADC_SingleEnded(HAL::hpPT);
         double voltage = (adc0 * 0.1875)/1000;
-        return Util::voltageToLowPressure(voltage);
+        return interpolate1000(voltage);
         // return 4;
     }
 
     float readLPPT() {
         int16_t adc0 = HAL::adc.readADC_SingleEnded(HAL::lpPT);
         double voltage = (adc0 * 0.1875)/1000;
-        return Util::voltageToLowPressure(voltage);
+        return interpolate1000(voltage);
         // return 3;
     }
 
